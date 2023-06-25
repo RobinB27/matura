796194e8cf1fde13046724a3cf9b748f27bcab70
Lucien Gees4 <lucienGees4@luciens-macbook-pro.home>
2023-06-25 13:17:53 +0200

    calculateEMA function works, MACD line calculations added, some small testing changes in testFile

--- TradingBot/MACDDecisionMaking.py
+++ TradingBot/MACDDecisionMaking.py
@@ -20,13 +20,14 @@ class MACDDecisionMaking:
         
         # MACD = 12 day EMA - 26 day EMA
         # EMA = exponential moving average
-        # simple moving average * (2/days of SMA + 1)
         
         # thought that maybe two modes arent needed because the code in mode = 0 should work for every date given (needs testing)
         # the above thought isnt possible due to the way the getStockPrice() function is implemented
         
         # mode is currently using the function param mode, shouldn't this be the class variable?
+        
         if mode == 0:
+            
             EMA_Placeholder12days = self.calculateEMA(12, portfolio, ticker)
             EMA_Placeholder26days = self.calculateEMA(26, portfolio, ticker)
             
@@ -35,10 +36,26 @@ class MACDDecisionMaking:
             
             
         elif mode == -1:
-            EMAplaceholder = self.calculateEMA(portfolio, ticker, dateStart)
+            
+            EMA_Placeholder12days = self.calculateEMA(12, portfolio, ticker, -1, dateStart)
+            EMA_Placeholder26days = self.calculateEMA(26, portfolio, ticker, -1, dateStart)
+            
+            MACDline = EMA_Placeholder12days - EMA_Placeholder26days
      
     #lot of boilerplate can be removed by putting the while loop inside it's own function
     def calculateSMA(self, daysToCalculate: int, portfolio, ticker, mode = 0, dateToCalculate = "0"):
+        """calculates the SMA of a stock, variable time period, mode 0 for current time, mode -1 for past prices
+
+        Args:
+            daysToCalculate (int): _description_
+            portfolio (_type_): _description_
+            ticker (_type_): _description_
+            mode (int, optional): _description_. Defaults to 0.
+            dateToCalculate (str, optional): _description_. Defaults to "0".
+
+        Returns:
+            _type_: _description_
+        """
         
         SMA_Value = 0
         
@@ -151,13 +168,53 @@ class MACDDecisionMaking:
                         
                         
     def calculateEMA(self, daysToCalculate: int, portfolio, ticker, mode = 0, dateToCalculate = "0"):
+        """Calculates the EMA needed for MACD calculations
+
+        Args:
+            daysToCalculate (int): _description_
+            portfolio (_type_): _description_
+            ticker (_type_): _description_
+            mode (int, optional): _description_. Defaults to 0.
+            dateToCalculate (str, optional): _description_. Defaults to "0".
+
+        Returns:
+            _type_: _description_
+        """
+        
+        #EMA(today) = (Close(today) * α) + (EMA(yesterday) * (1 - α))
         
         weightMultiplier = 2 / (daysToCalculate + 1)
         
+        #could be optimised with keeping a running EMA calculation, this recalculates the EMA every time it's called
         if mode == 0:
+            
+            EMAValue = 0
+            
             SMA_Placeholder = self.calculateSMA(daysToCalculate, portfolio, ticker)
-        
+            
+            stockPrice = 0
+            for stock in portfolio.stocksHeld:
+                if stock.name == ticker:
+                    stockPrice = stock.getStockPrice()
+                    
+            EMAValue = (stockPrice * weightMultiplier) + (SMA_Placeholder * (1 - weightMultiplier))
+            
+            return EMAValue
+            
         elif mode == -1:
+            
+            EMAValue = 0
+            placeholderDate = portfolio.addDayToDate(dateToCalculate)
+            
             SMA_Placeholder = self.calculateSMA(daysToCalculate, portfolio, ticker, -1,  dateToCalculate)
+            
+            stockPrice = 0
+            for stock in portfolio.stocksHeld:
+                if stock.name == ticker:
+                    stockPrice = stock.getStockPrice(-1, dateToCalculate, placeholderDate)
+                    
+            EMAValue = (stockPrice * weightMultiplier) + (SMA_Placeholder * (1 - weightMultiplier))
+            
+            return EMAValue
             
         
