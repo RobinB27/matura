2c4c8a3712816ab2c3a27e4bd43d63d2e4bb9ff2
RobinB27 <robin.bacher1@gmail.com>
2023-10-14 15:45:55 +0200

    rundata #3

--- test.py
+++ test.py
@@ -10,4 +10,4 @@ from TradingBot.Strategies.BuyAndHoldDM import BuyAndHoldDM
 # Rarely crashes because getPrice yields None, also slow: needs optimisation
 #Testing.compareDMs(100, SimpleSentimentDM, MACDDM, periodLimits=100, funds=10000)
 
-Testing.compareDMs(500, [SimpleSentimentDM, BuyAndHoldDM], 10000, startDate=None, stockList=None, periodLimits=100)
+Testing.compareDMs(500, [AvgSentimentDM, BuyAndHoldDM], 10000, startDate=None, stockList=None, periodLimits=100)
